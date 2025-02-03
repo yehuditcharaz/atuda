a13684ad3f17ade1f2421930de944c2564629a83
Y. Charaz <yeuditc@skyvar.co.il>
2025-02-03 11:54:50 +0200

    :green_heart: build rag-server image

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,13 +8,16 @@ fi
 
 echo "🤖 $GOOGLE_CREDENTIALS"
 echo "$GOOGLE_CREDENTIALS" | sed "s/^'//;s/'$//" > /project-for-version.json
-echo "$GOOGLE_CREDENTIALS" | sed "s/^'//;s/'$//" | grep -q '{.*}' && echo "Valid JSON" || echo "Invalid JSON"
 echo "✏️ service account writed successfully"
 gcloud auth activate-service-account --key-file=/project-for-version.json
 
 echo "🔐 after service account authorization"
 
+echo "📁 Listing files and directories in the root:"
+ls -la /
+
 echo "🚚 run the code"
+
 python -m src.services.main
 
 
