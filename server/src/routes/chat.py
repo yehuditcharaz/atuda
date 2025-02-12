c61bf1fa371fa9044533278b18169cefef16d897
Y. Charaz <yeuditc@skyvar.co.il>
2025-02-12 11:27:08 +0200

    :construction_worker: Add server image

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -1,32 +1,62 @@
-from flask import Flask
-from flask_socketio import SocketIO, emit
+# from flask import Flask
+# from flask_socketio import SocketIO, emit
 
+# from services.chain_multimodal import chain_multimodal_rag
+# from utils.config import UtilsConfig
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+
+
+# @app.route('/chat')
+# @socketio.on('chat')
+# def chat(query: str):
+#     try:
+#         result = chain_multimodal_rag.invoke(query)
+#         response = {
+#             'status_code': 200,
+#             'answer': result['answer'],
+#             'links': result['links'],
+#             'imgs': result['imgs']
+#         }
+#     except Exception as e:
+#         response = {
+#             'status_code': 500,
+#             'error': str(e)
+#         }
+#     emit('chat_answer', response)
+
+
+# if __name__ == '__main__':
+#     socketio.run(app, host=UtilsConfig.HOST, port=8080, allow_unsafe_werkzeug=True)
+
+from flask import Flask, request, jsonify
 from services.chain_multimodal import chain_multimodal_rag
 from utils.config import UtilsConfig
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 
 @app.route('/chat')
-@socketio.on('chat')
-def chat(query: str):
+def chat():
+    query = request.args.get('query')
     try:
         result = chain_multimodal_rag.invoke(query)
+        print(query)
         response = {
-            'status_code': 200,
+            'status_code':200,
             'answer': result['answer'],
             'links': result['links'],
-            'imgs': result['imgs']
+            'images': result['imgs']
         }
     except Exception as e:
         response = {
             'status_code': 500,
             'error': str(e)
         }
-    emit('chat_answer', response)
+    return jsonify(response)
+
 
 
 if __name__ == '__main__':
-    socketio.run(app, host=UtilsConfig.HOST, port=8080, allow_unsafe_werkzeug=True)
-    # socketio.run(app, host=UtilsConfig.HOST, port=8000)
+    app.run(host=UtilsConfig.HOST, port=UtilsConfig.PORT)
