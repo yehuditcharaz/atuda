f3331a6fcc030b71a2c9d35cd50567f41ac7c646
Y. Charaz <yeuditc@skyvar.co.il>
2025-03-05 11:38:55 +0200

    :zap: Build all images

---
+++ services/server-try/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class UtilsConfig:
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
