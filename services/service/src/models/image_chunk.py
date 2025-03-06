45eaecb6855ca3ff611f531b4937f0ff9f842ec6
Y. Charaz <yeuditc@skyvar.co.il>
2025-03-06 15:21:18 +0200

    :construction_worker: Build serverw image

--- services/service/src/models/image_chunk.py
+++
@@ -1,7 +0,0 @@
-from models.base_chunk import BaseChunk
-
-
-class ImageChunk(BaseChunk):
-    def __init__(self, id, content, url):
-        super().__init__(id, content)
-        self.url = url
--- services/service/src/services/data_preparation.py
+++
@@ -1,9 +0,0 @@
-from services.data_storage import store_data
-from services.extraction import get_chunks
-from services.summarization import set_summaries
-
-
-def data_preparing(bucket_name, file_list, folder_name, local_directory):
-    chunks = get_chunks()
-    set_summaries(chunks)
-    store_data(chunks)
