d1ac051fa48e4da082f7876a023e9caec857535f
Y. Charaz <yeuditc@skyvar.co.il>
2025-03-06 11:54:05 +0200

    :construction_worker: Build serverwwww image

--- services/RAG-service/entrypoint.sh
+++ services/RAG-service/entrypoint.sh
@@ -6,10 +6,10 @@ if [ -z "$GOOGLE_CREDENTIALS" ]; then
     echo "🤖 ERROR: GOOGLE_CREDENTIALS is not set."
     exit 1
 fi
-echo "$GOOGLE_CREDENTIALS" | sed "s/^'//;s/'$//" > /project-for-version.json
+echo "$GOOGLE_CREDENTIALS" | sed "s/^'//;s/'$//" > /key.json
 echo "✏️ service account writed successfully"
 
-gcloud auth activate-service-account --key-file=/project-for-version.json
+gcloud auth activate-service-account --key-file=/key.json
 echo "🔐 after service account authorization"
 
 echo "🚚 run the code"
