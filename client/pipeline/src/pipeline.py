380ad1f1411946eaa99b7d60d5f407ee44038fee
Y. Charaz <yeuditc@skyvar.co.il>
2025-03-09 08:50:04 +0200

    :green_heart: Build server image

--- client/pipeline/src/pipeline.py
+++ client/pipeline/src/pipeline.py
@@ -8,7 +8,8 @@ load_dotenv()
 
 class Pipeline:
     class Valves(BaseModel):
-        SERVER_URL: str = os.getenv("SERVER_URL")
+        # SERVER_URL: str = os.getenv("SERVER_URL")
+        SERVER_URL: str = "https://rag-service-633427059080.us-central1.run.app"
 
     def __init__(self):
         self.name = 'Ofer Chat'
